69ad4cc67634d7056c2002e3027754aaf75233d9
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-03-05 20:54:12 +0700

    Fix invalid api route

--- whitegate.go
+++ whitegate.go
@@ -1,17 +1,17 @@
 package whitegate
 
 import (
-    "strconv"
+	"strconv"
 
-    "gopkg.in/kataras/iris.v6"
-    "gopkg.in/kataras/iris.v6/adaptors/httprouter"
-    "gopkg.in/mgo.v2"
+	"gopkg.in/kataras/iris.v6"
+	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
+	"gopkg.in/mgo.v2"
 
-    "fmt"
+	"fmt"
 
-    "github.com/insthync/whitegate/apis"
-    "github.com/insthync/whitegate/configurations"
-    "github.com/insthync/whitegate/databases/mongodb"
+	"github.com/insthync/whitegate/apis"
+	"github.com/insthync/whitegate/configurations"
+	"github.com/insthync/whitegate/databases/mongodb"
 )
 
 var appConfig *configurations.AppConfiguration
@@ -19,37 +19,37 @@ var dbSession *mgo.Session
 
 // Start ... Start the server api
 func Start() {
-    defer clean()
-    var err error
-    appConfig, err = configurations.ReadConfig()
+	defer clean()
+	var err error
+	appConfig, err = configurations.ReadConfig()
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    dbSession, err = mongodb.Init(appConfig)
-    if err != nil {
-        panic(err)
-    }
+	dbSession, err = mongodb.Init(appConfig)
+	if err != nil {
+		panic(err)
+	}
 
-    app := iris.New()
-    app.Adapt(iris.DevLogger()) // adapt a logger which prints all errors to the os.Stdout
-    app.Adapt(httprouter.New()) // adapt the adaptors/httprouter or adaptors/gorillamux
+	app := iris.New()
+	app.Adapt(iris.DevLogger()) // adapt a logger which prints all errors to the os.Stdout
+	app.Adapt(httprouter.New()) // adapt the adaptors/httprouter or adaptors/gorillamux
 
-    app.Post("/login", apis.Login)
-    app.Post("/loginWithFacebook", apis.LoginWithFacebook)
-    app.Post("/register", apis.Register)
-    app.Post("/bindFacebookAccount", apis.BindFacebookAccount)
-    app.Post("/unbindFacebookAccount", apis.UnbindFacebookAccount)
-    app.Post("/unbindFacebookAccount", apis.UnbindFacebookAccount)
+	app.Post("/login", apis.Login)
+	app.Post("/loginWithFacebook", apis.LoginWithFacebook)
+	app.Post("/register", apis.Register)
+	app.Post("/bindFacebookAccount", apis.BindFacebookAccount)
+	app.Post("/unbindFacebookAccount", apis.UnbindFacebookAccount)
+	app.Post("/validateLogin", apis.ValidateLogin)
 
-    // Start the server
-    app.Listen(":" + strconv.Itoa(appConfig.Port))
+	// Start the server
+	app.Listen(":" + strconv.Itoa(appConfig.Port))
 }
 
 func clean() {
-    fmt.Println("Cleaning...")
-    if dbSession != nil {
-        dbSession.Close()
-    }
+	fmt.Println("Cleaning...")
+	if dbSession != nil {
+		dbSession.Close()
+	}
 }
